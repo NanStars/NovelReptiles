62cae1a856d937db56292cc4d39c37093ab0a7eb
NanStars <scanty.white@foxmail.com>
2021-08-18 23:44:58 +0800

    自定义下载书籍

--- 小说爬虫.py
+++ 小说爬虫.py
@@ -1,9 +1,16 @@
 import requests
 from lxml import etree
 
-url = "http://www.fanyibbs.com/artdetail_32486.html"
-response = requests.get(url)
-response.encoding = "utf-8"
+print('请输入小说序号')
+a = input()
+url = "http://www.fanyibbs.com"  # /artdetail_32486.html
+path = a
+b = '/artdetail_'
+c = '.html'
+URL = url + b + a + c
+# URL = parse.urljoin(url, path)  # 地址拼接
+response = requests.get(URL)
+response.encoding = "utf-8"  # 编码转换
 
 html = response.text
 ele = etree.HTML(html)
@@ -13,12 +20,14 @@ book_names = ele.xpath("//div[@class='content']/p/text()")  # 标签筛选规则
 s = ''
 
 
-def remove_upprintable_chars(s):
-    return ''.join(x for x in s if x.isprintable())
+def remove_upprintable_chars(t):
+    return ''.join(x for x in t if x.isprintable())
 
 
+print("请输入书籍名字下载")
+e = input()
 for book_name in range(len(book_names)):
     s += book_names[book_name] + '\n'
-with open('111112.txt', 'w', encoding='utf-8') as file:
+with open('{0}.txt'.format(e), 'w', encoding='utf-8') as file:
     file.writelines(s)
 print("输入完成")
