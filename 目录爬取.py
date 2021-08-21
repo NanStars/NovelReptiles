ea9badd7e254bfb391b2d0086a3f330cdfd4dd82
NanStars <scanty.white@foxmail.com>
2021-08-21 23:22:33 +0800

    笔趣阁目录爬取

---
+++ 目录爬取.py
@@ -0,0 +1,20 @@
+import requests
+from lxml import etree
+url = "https://www.xbiquge.la/xiaoshuodaquan/"
+response = requests.get(url)
+response.encoding="utf-8"
+
+html = response.text
+
+
+#ele
+ele = etree.HTML(html)
+book_names = ele.xpath("//div[@id='main']/div[@class='novellist']/ul/li/a/text()")
+book_urls = ele.xpath("//div[@id='main']/div[@class='novellist']/ul/li/a/@href")
+s = ''
+
+for book_name in range(len(book_names)):
+     s += book_names[book_name] + '\n' + book_urls[book_name] + '\n'
+with open('title.txt','w') as file:
+     file.writelines(s)
+print("输入完成")
